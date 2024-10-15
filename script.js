5a96810c2d4a90d7de4bffd3393bd3a0d99bc3ef
dreniko <156558640+dreniko@users.noreply.github.com>
2024-10-15 17:40:31 -0300

    Added the logic to get the computer choice to the script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const RandomNum = Math.random();
+
+    if (RandomNum < 0.33) {
+        return "rock";
+    } else if (RandomNum < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
